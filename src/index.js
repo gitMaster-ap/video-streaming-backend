7322eac0afa1313701e5f322d4b732fb9c7019df
ankush.truemeds <ankush.pachpor@truemeds.in>
2024-01-21 21:18:15 +0530

    feat : import db connection + env config

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import app from "./app.js";
+
+dotenv.config({
+  path: "../.env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongod DB connection failed !!!: ", error);
+    process.exit(1);
+  });
