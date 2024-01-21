9b792ee954c186fd19044d37eb31b635b81cdd29
ankush.truemeds <ankush.pachpor@truemeds.in>
2024-01-21 21:17:35 +0530

    Feat : add database configuration

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}`
+    );
+    console.log("MongoDB connected: ", connectionInstance.connection.host);
+  } catch (error) {
+    console.log("Mongod DB connection error: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
