de038758d5474b280f4b91e72c8848908b4b0aa6
ankush.truemeds <ankush.pachpor@truemeds.in>
2024-02-05 13:00:43 +0530

    Feat : Implement verifyToken middleware

---
+++ src/middlewares/auth.middlleware.js
@@ -0,0 +1,29 @@
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+// In JavaScript, the underscore (_) is often used as a convention to indicate that the parameter is not going to be used within the function body.
+export const verifyJWTToken = asyncHandler(async (req, _, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken "
+    );
+    if (!user) {
+      throw new ApiError(401, "Invalid access token");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
+});
