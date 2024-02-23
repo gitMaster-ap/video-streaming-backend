b293cb091ac5b1491e1c138eccf36218410a40d7
ankush.truemeds <ankush.pachpor@truemeds.in>
2024-02-24 00:03:45 +0530

    Fix : add json limit to 50 mb and import video routes

--- src/app.js
+++ src/app.js
@@ -6,15 +6,17 @@ const app = express();
 
 // middlewares
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "150mb" }));
 app.use(express.static("/public/tmp"));
 app.use(cookieParser());
 
 // routes import
 import userRoutes from "./routes/user.routes.js";
+import videoRoutes from "./routes/video.routes.js";
 
 // routes declaration
 app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/videos", videoRoutes);
 
 export default app;
