0793eea2f388c56d37e27becaf2edf3ec807f591
ankush.truemeds <ankush.pachpor@truemeds.in>
2024-02-27 11:16:57 +0530

    implement dashboard routes

---
+++ src/routes/dashboard.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { verifyJWTToken } from "../middlewares/auth.middleware.js";
+import { getChannelStats } from "../controllers/dashboard.controller.js";
+
+const router = Router();
+router.use(verifyJWTToken); // Apply verifyJWT middleware to all routes in this file
+
+router.route("/getChannelStats").get(getChannelStats);
+
+export default router;
