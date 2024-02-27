18b5cd88e3d950609ce1900385aedf0cc22d9397
ankush.truemeds <ankush.pachpor@truemeds.in>
2024-02-27 11:13:49 +0530

    Feat : add dashboard and subscription route to app

--- src/app.js
+++ src/app.js
@@ -14,9 +14,12 @@ app.use(cookieParser());
 // routes import
 import userRoutes from "./routes/user.routes.js";
 import videoRoutes from "./routes/video.routes.js";
-
+import dashboardRoutes from "./routes/dashboard.routes.js";
+import subscriptionRoutes from "./routes/subscription.routes.js";
 // routes declaration
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/videos", videoRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
+app.use("/api/v1/subscription", subscriptionRoutes);
 
 export default app;
