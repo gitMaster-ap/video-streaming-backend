5be544babcca92c7148cd8e6bd5194fd81d5d7dd
ankush.truemeds <ankush.pachpor@truemeds.in>
2024-03-03 21:04:26 +0530

    Refactor : optimize upload file on s3

--- src/utils/s3BucketUpload.js
+++ src/utils/s3BucketUpload.js
@@ -1,7 +1,6 @@
 import fs from "fs";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { ApiError } from "./apiError.js";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import path from "path";
 
 const s3Client = new S3Client({
@@ -12,24 +11,22 @@ const s3Client = new S3Client({
   },
 });
 
-const uploadOnS3 = async (file) => {
-  const fileName = path.basename(file);
+const uploadOnS3 = async ({ file, fileType }) => {
   try {
+    const fileName = path.basename(file);
+    const fileKey = Date.now() + "-" + fileName;
     const uploadParams = {
       Bucket: process.env.AWS_BUCKET_NAME,
-      Key: fileName,
+      Key: fileKey,
       Body: fs.createReadStream(file),
+      ContentType: fileType,
     };
-
     const data = await s3Client.send(new PutObjectCommand(uploadParams));
-
     fs.unlink(file, (err) => {
       if (err) console.error(`Error deleting file ${file}:`, err);
       else console.log(`Successfully deleted local file ${file}`);
     });
-
-    const fileUrl = `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileName}`;
-    return { data, fileUrl };
+    return { data, fileKey };
   } catch (error) {
     fs.unlink(file, (err) => {
       if (err) console.error(`Error deleting file ${file}:`, err);
